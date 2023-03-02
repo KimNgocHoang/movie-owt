5d6f4a70803405b25000014c69e69e55c4be3af4
Kim Ngoc <hoangkimngoc44@gmail.com>
2023-03-02 11:40:01 +0700

    delete level list & details

--- src/app/features/movies/movies.module.ts
+++ src/app/features/movies/movies.module.ts
@@ -2,10 +2,10 @@ import { ShareModule } from './../../share/share.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MoviesListItemComponent } from './components/movies-list-item/movies-list-item.component';
-import { MovieDetailsComponent } from './pages/details/movie-details/movie-details.component';
-import { TvShowDetailsComponent } from './pages/details/tv-show-details/tv-show-details.component';
-import { MovieListComponent } from './pages/list/movie-list/movie-list.component';
-import { TvShowListComponent } from './pages/list/tv-show-list/tv-show-list.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+import { TvShowDetailsComponent } from './pages/tv-show-details/tv-show-details.component';
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { TvShowListComponent } from './pages/tv-show-list/tv-show-list.component';
 
 @NgModule({
   declarations: [
