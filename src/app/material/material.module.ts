d65b694dbf5bd1b23bca1755097aac982110b545
Kim Ngoc <hoangkimngoc44@gmail.com>
2023-03-06 16:35:05 +0700

    change nav to mat-toolbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [ToolbarComponent],
+  declarations: [],
   imports: [CommonModule, MatToolbarModule, MatIconModule],
   exports: [MatToolbarModule, MatIconModule],
 })
