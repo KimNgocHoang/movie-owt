98307a8ed358bdcf7456dd3d4ad3d7d541379895
Kim Ngoc <hoangkimngoc44@gmail.com>
2023-03-15 10:34:52 +0700

    again set value default

--- src/app/share/components/skeleton/skeleton.component.ts
+++ src/app/share/components/skeleton/skeleton.component.ts
@@ -1,17 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-skeleton',
   templateUrl: './skeleton.component.html',
   styleUrls: ['./skeleton.component.scss'],
 })
-export class SkeletonComponent implements OnInit {
+export class SkeletonComponent {
   @Input() shape: 'image' | 'text';
-  @Input() width: string;
-  @Input() height: string;
-
-  ngOnInit() {
-    this.width = this.width ? this.width : '100%';
-    this.height = this.height ? this.height : '100%';
-  }
+  @Input() width: string = '100%';
+  @Input() height: string = '100%';
 }
