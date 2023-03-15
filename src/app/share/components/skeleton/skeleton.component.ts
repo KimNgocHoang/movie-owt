c20ec04487f97e0d726e17e880eb6a56493f7a33
Kim Ngoc <hoangkimngoc44@gmail.com>
2023-03-15 10:23:51 +0700

    fix

--- src/app/share/components/skeleton/skeleton.component.ts
+++ src/app/share/components/skeleton/skeleton.component.ts
@@ -1,12 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skeleton',
   templateUrl: './skeleton.component.html',
   styleUrls: ['./skeleton.component.scss'],
 })
-export class SkeletonComponent {
+export class SkeletonComponent implements OnInit {
   @Input() shape: 'image' | 'text';
   @Input() width: string;
   @Input() height: string;
+
+  ngOnInit() {
+    this.width = this.width ? this.width : '100%';
+    this.height = this.height ? this.height : '100%';
+  }
 }
