1cde6cccd15e1d313c593c16899da12c76d77cf8
Kim Ngoc <hoangkimngoc44@gmail.com>
2023-03-24 20:25:48 +0700

    use pass params into translateService.instant

--- src/app/material/TranslatedMatPaginator.ts
+++ src/app/material/TranslatedMatPaginator.ts
@@ -7,21 +7,21 @@ export class TranslatedMatPaginator extends MatPaginatorIntl {
   rangeLabel = 'of';
   constructor(private translateService: TranslateService) {
     super();
-    this.getAndInitTranslations();
   }
 
   getRangeLabel = (page: number, pageSize: number, length: number) => {
     this.getAndInitTranslations();
     if (length === 0 || pageSize === 0) {
-      return `${this.translateService.instant(
-        'shows.movieList.labels.page'
-      )} 0 ${this.rangeLabel} ${length}`;
+      return `${this.translateService.instant('shows.movieList.labels.page', {
+        page: 0,
+        totalPage: length,
+      })}`;
     }
-    return `${this.translateService.instant('shows.movieList.labels.page')} ${
-      page + 1
-    } ${this.rangeLabel} ${length}`;
+    return `${this.translateService.instant('shows.movieList.labels.page', {
+      page: page + 1,
+      totalPage: length,
+    })}`;
   };
-
   getAndInitTranslations() {
     this.itemsPerPageLabel = this.translateService.instant(
       'shows.movieList.labels.itemsPerPage'
