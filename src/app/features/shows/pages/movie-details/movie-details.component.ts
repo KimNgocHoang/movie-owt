8b3ef2aa933155eb99806adc776a0075de9412bb
Kim Ngoc <hoangkimngoc44@gmail.com>
2023-03-28 09:08:07 +0700

    rename according to BEM

--- src/app/features/shows/pages/movie-details/movie-details.component.ts
+++ src/app/features/shows/pages/movie-details/movie-details.component.ts
@@ -1,5 +1,6 @@
+import { Subscription } from 'rxjs';
 import { Movie } from './../../../../core/models/movie';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { MovieService } from '../../movie.service';
 
@@ -8,18 +9,19 @@ import { MovieService } from '../../movie.service';
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss'],
 })
-export class MovieDetailsComponent implements OnInit {
+export class MovieDetailsComponent implements OnInit, OnDestroy {
   movie: Movie;
   genres: string;
   loading = true;
+  getMovieByApiSub: Subscription;
 
   constructor(
     private movieService: MovieService,
-    private route: ActivatedRoute,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
+    this.getMovieByApiSub = this.route.params.subscribe((params: Params) => {
       this.movie = this.getMovie(+params['id']);
     });
   }
@@ -32,5 +34,9 @@ export class MovieDetailsComponent implements OnInit {
       this.loading = false;
     });
     return this.movie;
+  }
+
+  ngOnDestroy(): void {
+    this.getMovieByApiSub.unsubscribe();
   }
 }
